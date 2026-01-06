3b947894ff7c6efc192d9cacf11b2bfa43f71ef9
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2026-01-06 19:49:12 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='currenteventstokg',
+    version='0.2.0',
+    packages=['currenteventstokg'],
+    url='',
+    license='',
+    author='Lars Michaelis, Patrick Westphal',
+    author_email='patrick.westphal@uni-hamburg.de',
+    description='',
+    install_requires=[
+        'OSMPythonTools==0.3.5',
+        'rdflib==6.2.0'
+    ]
+)
