b296081085ace2fbc64cdf83508717723b601612
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2026-01-07 14:01:40 +0100

    Simplify etc

---
+++ currenteventstokg/util.py
@@ -0,0 +1,51 @@
+# Copyright: (c) 2022, Lars Michaelis
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from typing import List
+
+months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+month_name_to_int = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
+}
+
+def graph_name_list(start: int, end: int) -> List[str]:
+    graph_names = []
+    current_index: int = start
+
+    while current_index <= end:
+        month = int(current_index%100)
+        year = int((current_index-month)/100)
+        graph_names.append(f'{months[month-1]}_{year}')
+        month += 1
+
+        if month > 12:
+            year += 1
+            month = 1
+
+        current_index = int(year*100+month)
+
+    return graph_names
