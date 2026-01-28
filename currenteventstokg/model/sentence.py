b81afaeb6528d68d125f8ec31fe59c501d135562
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2026-01-28 12:20:55 +0100

    Minor changes in model.sentence

--- currenteventstokg/model/sentence.py
+++ currenteventstokg/model/sentence.py
@@ -1,16 +1,17 @@
 # Copyright: (c) 2022, Lars Michaelis
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from currenteventstokg.objects.article import Article
-from currenteventstokg.objects.link import Link
+from typing import List
 
-from typing import List, Optional
+from currenteventstokg.model.article import Article
+from currenteventstokg.model.link import Link
+
 
 class Sentence:
-    def __init__(self, text: str, start: int, end: int, links: List[Link]):
+    def __init__(self, text: str, start_index: int, end_index: int, links: List[Link]):
         self.text: str = text
-        self.start: int = start
-        self.end: int = end
+        self.start_index: int = start_index
+        self.end_index: int = end_index
         self.links: List[Link] = links
     
     def get_linked_articles(self) -> List[Article]:
